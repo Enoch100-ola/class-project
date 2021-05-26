9a2c5ea3e3c7c918ce72f4aaf750e2b327aee2e7
Enoch <enochola96@gmail.com>
2021-05-26 12:53:17 +0100

    javascript added

---
+++ e-shop.js
@@ -0,0 +1,15 @@
+let phoneListContainer = document.querySelector('#phone-list > .row');
+let phoneListDetails = document.querySelectorAll('#phone-list .details');
+
+for (let index = 0; index < phoneList.length; index++) {
+  console.log(index);
+  phoneListContainer.innerHTML += `
+        <div class="col-md-4">
+            <div class="card shadow-primary">
+            <img class="card-img-top" src="${phoneList[index].imageUrl}">
+            <h5>${phoneList[index].name}</h5>
+            <a class="btn btn-info details" href="product-details.html?id=${index}">read more</a>
+            </div>
+        </div>
+    `;
+}
