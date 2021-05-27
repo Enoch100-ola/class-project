8762651b8f891e18381134b34fe05abd013fd52d
Enoch <enochola96@gmail.com>
2021-05-27 16:14:45 +0100

    no too much changes yet

--- e-shop.js
+++ e-shop.js
@@ -4,12 +4,13 @@ let phoneListDetails = document.querySelectorAll('#phone-list .details');
 for (let index = 0; index < phoneList.length; index++) {
   console.log(index);
   phoneListContainer.innerHTML += `
-        <div class="col-md-3">
+        <div class="col-md-3 my-5 mx-4 ">
             <div class="card shadow-primary">
             <img class="card-img-top" src="${phoneList[index].imageUrl}">
             <h5>${phoneList[index].name}</h5>
             <a class="btn btn-info details" href="product-details.html?id=${index}">read more</a>
             </div>
+           
         </div>
     `;
 }
